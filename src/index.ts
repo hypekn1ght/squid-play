de86537b2019def8af5594bc5ca08584d674f39a
A5Farmer <95952749+hypekn1ght@users.noreply.github.com>
2023-08-31 22:47:14 +1200

    current state

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-import { Squid, TokenData, ChainData } from "@0xsquid/sdk";
+import { Squid, TokenData, ChainData, ChainName } from "@0xsquid/sdk";
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 (async () => {
+
+  //************ WATCH OUT QUOTE ONLY */
+  const quoteOnly = false;
+  const integratorID = "none"; //"trustwallet-api"
+  const nativeSwap = true;
+  //************ WATCH OUT QUOTE ONLY */
+  
   // instantiate the SDK
   const squid = new Squid({
-    baseUrl: "https://testnet.api.squidrouter.com", // for mainnet use "https://api.0xsquid.com" "https://testnet.api.squidrouter.com"
-    integratorId: "your-integrator-id"
+    baseUrl: "https://squid-api-git-main-cosmos-mainnet-0xsquid.vercel.app", // for mainnet use "https://api.0xsquid.com" "https://testnet.api.squidrouter.com"
+    integratorId: integratorID
   });
 
-  squid.setConfig({
-    baseUrl: "https://testnet.api.squidrouter.com", // for mainnet use "https://api.0xsquid.com"
-    integratorId: "your-integrator-id"
-  });
+  // squid.setConfig({
+  //   baseUrl: "https://api.0xsquid.com", // for mainnet use "https://testnet.api.squidrouter.com"
+  //   integratorId: "your-integrator-id"
+  // });
 
   // init the SDK
   await squid.init();
@@ -25,24 +35,79 @@ import { Squid, TokenData, ChainData } from "@0xsquid/sdk";
   //       t.chainId === chains.find(c => c.chainName === "Ethereum")?.chainId
   // )
 
-  // console.log(`token found: ${JSON.stringify(fromToken, null, 2)}`)
+  const sourceChain = squid.chains.find(
+    c =>
+      c.chainName === ChainName.MOONBEAM
+  );
+  console.log(`source chain : ${sourceChain?.chainName} , ${sourceChain?.chainId}`);
 
+  const sourceToken = squid.tokens.find(
+    t => 
+      t.chainId == sourceChain?.chainId &&
+      t.symbol === "WAVAX"
+  )
+  if (nativeSwap) {
+    console.log("native swap")
+  } else {
+    console.log(`source token : ${sourceToken?.symbol} at ${sourceToken?.address}`);
+  }
+  
+  const destChain = squid.chains.find(
+    c =>
+      c.chainName === ChainName.ARBITRUM
+  );
+  console.log(`destination chain : ${destChain?.chainName} , ${destChain?.chainId}`);
+
+  const destToken = squid.tokens.find(
+    t => 
+      t.chainId == destChain?.chainId &&
+      t.symbol === "WGLMR"
+  );
+
+  if (nativeSwap) {
+    console.log("native swap")
+  } else {
+    console.log(`destination token : ${destToken?.symbol} at ${destToken?.address}`);
+  }
   const params = {
-    fromChain: 43113, // Avalanche Fuji Testnet
-    fromToken: "0x57f1c63497aee0be305b8852b354cec793da43bb", // aUSDC on Avalanche Fuji Testnet
-    fromAmount: "50000000000000000", // 0.05 WETH
-    toChain: 5, // Goerli testnet
-    toToken: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6", // WETH on Goerli
-    toAddress: "0xAD3A87a43489C44f0a8A33113B2745338ae71A9D", // the recipient of the trade
+    fromChain: sourceChain!.chainId, // avalanche
+    fromToken: nativeSwap ? "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" : sourceToken!.address, // native : 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
+    fromAmount: "30000000000000000000", //60 glmr 
+    toChain: destChain!.chainId, // 
+    toToken: nativeSwap ? "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" : destToken!.address, // native : 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
+    toAddress: "0xE5ebC33267Dda14a1eEFf4d09eaEAF8032f8F188", // the recipient of the trade
     slippage: 1.00, // 1.00 = 1% max slippage across the entire route
     enableForecall: true, // instant execution service, defaults to true
-    quoteOnly: true, // optional, defaults to false
+    quoteOnly: quoteOnly, // optional, defaults to false
     collectFees: {
-      integratorAddress: "0x0x010a87dF2aE2B729DE4F8562B107DD24a549a6dF", 
-      fee: 5
+      integratorAddress: "0xE5ebC33267Dda14a1eEFf4d09eaEAF8032f8F188",
+      fee: 10
     }
   };
   console.log("starting route gen")
-  const {route} = await squid.getRoute(params);
-  console.log(`route: ${JSON.stringify(route, null, 2)}`);
+  const { route } = await squid.getRoute(params);
+  console.log(`fees: ${JSON.stringify(route.estimate.feeCosts, null, 2)}`);
+
+  console.log(route.estimate.toAmount);
+
+  if (!quoteOnly) {
+    // console.log(`token found: ${JSON.stringify(fromToken, null, 2)}`)
+    const PK = process.env.PRIVATE_KEY!;
+    const provider = new ethers.providers.JsonRpcProvider(sourceChain?.rpc);
+    const signer = new ethers.Wallet(PK, provider);
+    const tx = (await squid.executeRoute({
+      signer,
+      route,
+    })) as ethers.providers.TransactionResponse;
+    const txReceipt = await tx.wait();
+
+    const axelarScanLink =
+      "https://axelarscan.io/gmp/" + txReceipt.transactionHash;
+    console.log(
+      "Finished! Please check Axelarscan for more details: ",
+      axelarScanLink,
+      "\n"
+    );
+  }
+
 })();
