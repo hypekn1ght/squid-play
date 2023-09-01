94bcc4152091152043fd22cdec3e250069796f9a
A5Farmer <95952749+hypekn1ght@users.noreply.github.com>
2023-09-01 19:56:46 +1200

    cosmos test

--- src/index.ts
+++ src/index.ts
@@ -6,83 +6,59 @@ dotenv.config();
 (async () => {
 
   //************ WATCH OUT QUOTE ONLY */
-  const quoteOnly = false;
-  const integratorID = "none"; //"trustwallet-api"
-  const nativeSwap = true;
+  const quoteOnly = true;
+  const integratorID = "trustwallet-api"; //""
+  const nativeSwap = false;
   //************ WATCH OUT QUOTE ONLY */
   
   // instantiate the SDK
-  const squid = new Squid({
-    baseUrl: "https://squid-api-git-main-cosmos-mainnet-0xsquid.vercel.app", // for mainnet use "https://api.0xsquid.com" "https://testnet.api.squidrouter.com"
-    integratorId: integratorID
-  });
+  const squid = new Squid();
 
-  // squid.setConfig({
-  //   baseUrl: "https://api.0xsquid.com", // for mainnet use "https://testnet.api.squidrouter.com"
-  //   integratorId: "your-integrator-id"
-  // });
+  squid.setConfig({
+    baseUrl: "https://squid-api-git-main-cosmos-mainnet-0xsquid.vercel.app", // for mainnet use "https://api.0xsquid.com" "https://testnet.api.squidrouter.com"
+    integratorId: "trustwallet-api"
+  });
 
   // init the SDK
   await squid.init();
   console.log("Squid inited");
 
-  // const tokens = squid.tokens as TokenData[]
-  // const chains = squid.chains as ChainData[]
-
-  // const fromToken = tokens.find(
-  //     t => 
-  //       t.symbol === "USDC" &&
-  //       t.chainId === chains.find(c => c.chainName === "Ethereum")?.chainId
-  // )
-
   const sourceChain = squid.chains.find(
     c =>
-      c.chainName === ChainName.MOONBEAM
+      c.chainName === ChainName.AVALANCHE
   );
   console.log(`source chain : ${sourceChain?.chainName} , ${sourceChain?.chainId}`);
-
-  const sourceToken = squid.tokens.find(
-    t => 
-      t.chainId == sourceChain?.chainId &&
-      t.symbol === "WAVAX"
-  )
-  if (nativeSwap) {
-    console.log("native swap")
-  } else {
-    console.log(`source token : ${sourceToken?.symbol} at ${sourceToken?.address}`);
-  }
   
   const destChain = squid.chains.find(
     c =>
-      c.chainName === ChainName.ARBITRUM
+      c.chainName === ChainName.OSMOSIS
   );
   console.log(`destination chain : ${destChain?.chainName} , ${destChain?.chainId}`);
 
-  const destToken = squid.tokens.find(
-    t => 
-      t.chainId == destChain?.chainId &&
-      t.symbol === "WGLMR"
-  );
-
-  if (nativeSwap) {
-    console.log("native swap")
-  } else {
-    console.log(`destination token : ${destToken?.symbol} at ${destToken?.address}`);
-  }
   const params = {
     fromChain: sourceChain!.chainId, // avalanche
-    fromToken: nativeSwap ? "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" : sourceToken!.address, // native : 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
-    fromAmount: "30000000000000000000", //60 glmr 
+    // fromToken: squid.tokens.find(
+    //   t => 
+    //     t.chainId == sourceChain?.chainId &&
+    //     t.symbol == "axlUSDC"
+    // )!.address, 
+    fromToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    fromAmount: "500000000000000000", // 0.1
     toChain: destChain!.chainId, // 
-    toToken: nativeSwap ? "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" : destToken!.address, // native : 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
-    toAddress: "0xE5ebC33267Dda14a1eEFf4d09eaEAF8032f8F188", // the recipient of the trade
+    // toToken: squid.tokens.find(
+    //   t => 
+    //     t.chainId == destChain?.chainId &&
+    //     t.symbol == "axlUSDC"
+    // )!.address, 
+    toToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    toAddress: "osmo1czjgvx8m2579zf5qeus3z59pgn6d6722kcphyq", // the recipient of the trade
     slippage: 1.00, // 1.00 = 1% max slippage across the entire route
     enableForecall: true, // instant execution service, defaults to true
     quoteOnly: quoteOnly, // optional, defaults to false
-    collectFees: {
-      integratorAddress: "0xE5ebC33267Dda14a1eEFf4d09eaEAF8032f8F188",
-      fee: 10
-    }
+    // collectFees: {
+    //   integratorAddress: "0xE5ebC33267Dda14a1eEFf4d09eaEAF8032f8F188",
+    //   fee: 10
+    // }
   };
   console.log("starting route gen")
   const { route } = await squid.getRoute(params);
